fixed input partial map training using multiple GPUs and add rotation

data_generator_input_partial_map.py
@@ -17,6 +17,9 @@ import pickle
 from skimage.morphology import skeletonize
 import torch
 import math
+from scipy import ndimage
+import bz2
+import _pickle as cPickle
 
 def get_region(robot_pos, H, W, size=2):
 	y, x = robot_pos
@@ -231,6 +234,17 @@ class Data_Gen_MP3D:
 			#print(f'end U_a.shape = {U_a.shape}')
 			#print(f'end U_d.shape = {U_d.shape}')
 
+			# rotate
+			rot = random.choice((0, 45, 90, 135, 180, -45, -90, -135))
+			for i in range(M_p.shape[0]):
+				M_p[i] = ndimage.rotate(M_p[i], rot, reshape=False)
+				M_p[i] = np.where(M_p[i] < 1, 0, M_p[i])
+			U_a = ndimage.rotate(U_a, rot, reshape=False)
+			U_a = np.where(U_a < 1, 0, U_a)
+			for i in range(U_d.shape[2]):
+				U_d[:, :, i] = ndimage.rotate(U_d[:, :, i], rot, reshape=False)
+				U_d[:, :, i] = np.where(U_d[:, :, i] < 1, 0, U_d[:, :, i])
+
 			#=================================== visualize M_p =========================================
 			if cfg.PRED.PARTIAL_MAP.FLAG_VISUALIZE_PRED_LABELS:
 				occ_map_Mp = M_p[0]
@@ -285,9 +299,14 @@ class Data_Gen_MP3D:
 			eps_data['Ud'] = U_d.copy()
 
 			sample_name = str(count_sample).zfill(len(str(num_samples)))
-			np.save(f'{self.scene_folder}/{sample_name}.npy', eps_data)
-			with open(f'{self.scene_folder}/{sample_name}.pkl', 'wb') as pk_file:
-				pickle.dump(obj=frontiers, file=pk_file)
+			#np.save(f'{self.scene_folder}/{sample_name}.npy', eps_data)
+			#with open(f'{self.scene_folder}/{sample_name}.pkl', 'wb') as pk_file:
+			#pickle.dump(obj=frontiers, file=pk_file)
+			with bz2.BZ2File(f'{self.scene_folder}/{sample_name}.pbz2', 'w') as fp:
+				cPickle.dump(
+					eps_data,
+					fp
+				)
 			
 			#===================================================================
 			count_sample += 1

dataloader_input_partial_map.py
@@ -12,6 +12,8 @@ import os
 import glob
 import pickle
 from modeling.utils.baseline_utils import apply_color_to_map
+import bz2
+import _pickle as cPickle
 
 class MP3DSceneDataset(data.Dataset):
 
@@ -21,21 +23,31 @@ class MP3DSceneDataset(data.Dataset):
 		
 		self.saved_folder = f'{data_folder}/{self.split}/{self.scene_name}'
 
-		self.sample_name_list = [os.path.splitext(os.path.basename(x))[0] for x in sorted(glob.glob(f'{self.saved_folder}/*.npy'))] 
-
+		if self.split == 'val':
+			self.sample_name_list = [os.path.splitext(os.path.basename(x))[0] for x in sorted(glob.glob(f'{self.saved_folder}/*.npy'))] 
+		elif self.split == 'train':
+			self.sample_name_list = [os.path.splitext(os.path.basename(x))[0] for x in sorted(glob.glob(f'{self.saved_folder}/*.pbz2'))] 
 
 	def __len__(self):
 		return len(self.sample_name_list)
 
 	def __getitem__(self, i):
-		#============================= load npy file and pickle file ===============================
-		npy_file = np.load(f'{self.saved_folder}/{self.sample_name_list[i]}.npy', allow_pickle=True).item()
-		pk_file = pickle.load(open(f'{self.saved_folder}/{self.sample_name_list[i]}.pkl', 'rb'))
+		if self.split == 'val':
+			#============================= load npy file and pickle file ===============================
+			npy_file = np.load(f'{self.saved_folder}/{self.sample_name_list[i]}.npy', allow_pickle=True).item()
+			pk_file = pickle.load(open(f'{self.saved_folder}/{self.sample_name_list[i]}.pkl', 'rb'))
 
-		M_p = npy_file['Mp']
-		U_a = npy_file['Ua']
-		U_d = npy_file['Ud']
-		frontiers = pk_file
+			M_p = npy_file['Mp']
+			U_a = npy_file['Ua']
+			U_d = npy_file['Ud']
+			frontiers = pk_file
+		elif self.split == 'train':
+			with bz2.BZ2File(f'{self.saved_folder}/{self.sample_name_list[i]}.pbz2', 'rb') as fp:
+				npy_file = cPickle.load(fp)
+				M_p = npy_file['Mp']
+				U_a = npy_file['Ua']
+				U_d = npy_file['Ud']
+				frontiers = None
 		#print(f'U_a.shape = {U_a[..., np.newaxis].shape}')
 		#print(f'U_d.shape = {U_d.shape}')
 		U_all = np.concatenate((U_a[..., np.newaxis], U_d), axis=2)

frontier_explore_test_actions.py
@@ -20,6 +20,7 @@ from modeling.localNavigator_slam import localNav_slam
 from skimage.morphology import skeletonize
 from modeling.utils.UNet import UNet
 import torch
+from collections import OrderedDict
 
 split = 'test' #'test' #'train'
 env_scene = 'yqstnuAEVhm' #'17DRP5sb8fy' #'yqstnuAEVhm'
@@ -75,10 +76,15 @@ device = torch.device('cuda:0')
 if cfg.NAVI.PERCEPTION == 'UNet_Potential':
 	unet_model = UNet(n_channel_in=cfg.PRED.PARTIAL_MAP.INPUT_CHANNEL, n_class_out=cfg.PRED.PARTIAL_MAP.OUTPUT_CHANNEL).to(device)
 	if cfg.PRED.PARTIAL_MAP.INPUT == 'occ_and_sem':
-		checkpoint = torch.load(f'{cfg.PRED.PARTIAL_MAP.SAVED_FOLDER}/{cfg.PRED.PARTIAL_MAP.INPUT}/experiment_6/checkpoint.pth.tar')
+		checkpoint = torch.load(f'{cfg.PRED.PARTIAL_MAP.SAVED_FOLDER}/{cfg.PRED.PARTIAL_MAP.INPUT}/experiment_25/best_checkpoint.pth.tar')
 	elif cfg.PRED.PARTIAL_MAP.INPUT == 'occ_only':
 		checkpoint = torch.load(f'run/MP3D/unet/experiment_5/checkpoint.pth.tar')
-	unet_model.load_state_dict(checkpoint['state_dict'])
+
+	new_state_dict = OrderedDict()
+	for k, v in checkpoint['state_dict'].items():
+		name = k[7:] #remove 'module'
+		new_state_dict[name] = v
+	unet_model.load_state_dict(new_state_dict)
 
 LN = localNav_Astar(pose_range, coords_range, WH, scene_name)
 

train_UNet_input_partial_map.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from dataloader_input_partial_map import get_all_scene_dataset, my_collate
 import torch.utils.data as data
 import torch
+import torch.nn as nn
 from core import cfg
 from itertools import islice
 
@@ -72,7 +73,9 @@ dataloader_val = data.DataLoader(dataset_val,
 
 #================================================================================================================================
 # Define network
-model = UNet(n_channel_in=cfg.PRED.PARTIAL_MAP.INPUT_CHANNEL, n_class_out=cfg.PRED.PARTIAL_MAP.OUTPUT_CHANNEL).cuda()
+model = UNet(n_channel_in=cfg.PRED.PARTIAL_MAP.INPUT_CHANNEL, n_class_out=cfg.PRED.PARTIAL_MAP.OUTPUT_CHANNEL)
+model = nn.DataParallel(model)
+model = model.cuda()
 
 #=========================================================== Define Optimizer ================================================
 import torch.optim as optim
@@ -161,6 +164,13 @@ for epoch in range(cfg.PRED.PARTIAL_MAP.EPOCHS):
 		print('[Epoch: %d, numImages: %5d]' % (epoch, iter_num * cfg.PRED.PARTIAL_MAP.BATCH_SIZE))
 		print('Loss: %.3f' % test_loss)
 
+		saver.save_checkpoint({
+				'epoch': epoch + 1,
+				'state_dict': model.state_dict(),
+				'optimizer': optimizer.state_dict(),
+				'loss': test_loss,
+			}, filename='checkpoint.pth.tar')
+
 		#new_pred = mIoU
 		if test_loss < best_test_loss:
 			best_test_loss = test_loss
@@ -170,7 +180,7 @@ for epoch in range(cfg.PRED.PARTIAL_MAP.EPOCHS):
 				'state_dict': model.state_dict(),
 				'optimizer': optimizer.state_dict(),
 				'loss': test_loss,
-			})
+			}, filename='best_checkpoint.pth.tar')
 
 	scheduler.step()
 