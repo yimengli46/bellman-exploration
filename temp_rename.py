2f20634a4773cef6791617fe2012b44a9ab45146
yimengli46 <peter824360321@gmail.com>
2024-03-17 23:37:17 -0400

    reorganized the files for git repo appearance.

--- temp_rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folder = 'output/TESTING_RESULTS_Frontier/DP_large'
-num_files = 500
-
-
-for i in range(num_files):
-	os.rename(f"{folder}/step_{i}_semmap.png", f"{folder}/step_{i:03}_semmap.png")
-	os.rename(f"{folder}/step_{i}_panor.png", f"{folder}/step_{i:03}_panor.png")
